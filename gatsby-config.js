e91ca7393e93932af6cf01aba9d080fea5c28201
Bruno Arsene <arsene.bruno@gmail.com>
2019-03-13 21:30:17 +0100

    Add google analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-136242001-1`,
       },
     },
     `gatsby-plugin-feed`,
